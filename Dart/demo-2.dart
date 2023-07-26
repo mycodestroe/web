ccf0a8abb32aa84d0b3ae4aed34b87c6bed9721b
wangdelei <jimmy.wang@gaodun.com>
2023-07-26 14:39:53 +0800

    dart

---
+++ Dart/demo-2.dart
@@ -0,0 +1,13 @@
+// 表示main方法没有返回值
+void main() {
+  // var str = '你好dart';
+  // print(str);
+  // String str1 = '你好dart';
+  // print(str1);
+  // const PI = 3.14;
+  // final PI = 3.14 定义常量
+  // int a = 123;
+  // double b = 123.3;  既可以是整型也可以是浮点型
+  // bool flag = true;
+  // List 类型
+}
